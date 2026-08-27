70738fb71b235b47589c1f3891e508ee9679c4e8
Jameel Ahmed <adeeljameel810@gmail.com>
2026-08-27 09:21:56 +0500

    Refactor for loop to print multiples of 5
    Refactor code to improve readability and formatting.

--- for loop 3.cpp
+++ for loop 3.cpp
@@ -1,14 +1,18 @@
-//3.Print only multiples of 5 up to N.
-#include<iostream>
+// 3. Print only multiples of 5 up to N.
+#include <iostream>
 using namespace std;
-int main (){
-	int n;
-	cout<<"enter the value of n";
-	cin>>n;
-	for(int i=1;i<=n;i++){
-		if(i%5==0){
-					cout<<i<<endl;
-	}cout<<endl;
-	}
-	return 0;
+
+int main() {
+    int n;
+
+    cout << "Enter the value of N: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 5 == 0) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
 }
